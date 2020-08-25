7a52fe1df662a684841dcb61e4ec752bfee3a553
SEVERECLICK-dev <papadopouloskaris@gmail.com>
2020-08-25 13:00:24 +0200

    update boucle for

---
+++ exo_3_boucle_for/main.js
@@ -0,0 +1,7 @@
+for (let i = 0; i <= 20; i++) {
+    if (i % 2 == 0) {
+        console.log(i + " est un nombre pair. ");
+    } else {
+        console.log(i + " est un nombre impair. ");
+    }
+}
